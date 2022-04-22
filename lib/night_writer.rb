66e819c382a9e23ac1943048a90f0967bc41cee9
Gwendolyn Ruiz <gwen_marina@yahoo.com>
2022-04-22 14:15:00 -0700

    added files for project

---
+++ lib/night_writer.rb
@@ -0,0 +1,9 @@
+# $ ruby ./lib/night_writer.rb message.txt braille.txt
+# Created 'braille.txt' containing 256 characters
+
+
+# Create a Ruby program that prints the sample line of text provided above no matter what arguments are provided from the command line.
+# Update your existing program so that the name of the file that it prints out changes based on the second input that the user provides
+# from the command line.
+# Further update your program so that the number of characters it prints out changes based on the number of characters in the file that
+# the user provides as the first argument that the user provides from the command line.
